a34b1fd8ac133275fe014d184553b1060d40fadf
HungNguyen <110725585+Dephe1101@users.noreply.github.com>
2025-02-19 14:23:52 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "*",
+        origin: process.env.CORS_ORIGIN || "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
